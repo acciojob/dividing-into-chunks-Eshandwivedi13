e156fb13de867b5e68cafd5bdc4be84a950582e8
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-21 16:16:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
-const divide = (arr, n) => {
-  // Write your code here
-};
+const divide = (arr, n) => {// n - maxSum
+	//list of subarrays, sum of each subarray <= n
+	let ans = [];
+	let tempAns = [];
+	int sum = 0;
+	for(let elem of arr){
+		sum += elem;
+		if(sum > n){
+			ans.push(tempAns);
+			tempAns = [];
+			tempAns.push(elem);
+			continue;
+		}
+		tempAns.push(elem);
+	}
+	return ans;
+};    
 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
